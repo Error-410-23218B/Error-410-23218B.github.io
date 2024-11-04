581d39180077ed8a06be5f3b1a24f7e5a4d9e7c7
Rohan Jose <19rjose@thelangton.org.uk>
2024-11-04 19:12:15 +0000

    backToTop

--- js/carousel.js
+++ js/carousel.js
@@ -2,4 +2,9 @@ function goToCarousel(itemId) {
     var carousel = new bootstrap.Carousel(document.getElementById('myCarousel'));
     var itemIndex = Array.from(document.querySelectorAll('.carousel-item')).findIndex(item => item.id === itemId);
     carousel.to(itemIndex); // Navigate to the carousel item index
+    document.body.scrollTop=0;
+    document.documentElement.scrollTop=0;
+    event.preventDefault();
 }
+
+
